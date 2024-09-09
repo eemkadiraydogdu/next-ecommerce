9303c58cf4ba479c99a110b7d0f2cf9284df3519
00003688 <kadiraydogdu@tcdd.gov.tr>
2024-09-09 16:17:29 +0300

    .

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -6,8 +6,15 @@ import DOMPurify from "isomorphic-dompurify";
 
 const PRODUCT_PER_PAGE = 20;
 
-const ProductList = async ({categoryId, limit}:{categoryId:string; limit?:number}) => {
-
+const ProductList = async ({
+  categoryId, 
+  limit,
+  searchParams,
+}:{
+  categoryId:string; 
+  limit?:number;
+  searchParams?:any
+}) => {
   const wixClient = await wixClientServer();
   const res = await wixClient.products
     .queryProducts()
