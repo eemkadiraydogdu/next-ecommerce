1d9958097fe0d53bacc1b930e6cde58d09acee08
00003688 <kadiraydogdu@tcdd.gov.tr>
2024-11-24 21:29:04 +0300

    ...

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,4 +1,15 @@
-import UpdateButton from "@/components/UpdateButton";
+
+
+const ProfilePage = () => {
+  return (
+    <div>ProfilePage</div>
+  )
+}
+
+export default ProfilePage
+
+
+/* import UpdateButton from "@/components/UpdateButton";
 import { updateUser } from "@/lib/actions";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { members } from "@wix/members";
@@ -95,4 +106,4 @@ const ProfilePage = async () => {
   );
 };
 
-export default ProfilePage;
+export default ProfilePage; */
